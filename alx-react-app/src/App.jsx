8c53e4dae82cfaaff9c6a3c9dd08510ee9a5617d
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-02-11 20:32:38 +0000

    userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 import WelcomeMessage from './components/WelcomeMessage'
 
 
@@ -13,9 +14,10 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
-  )
+  );
 }
 
 export default App
