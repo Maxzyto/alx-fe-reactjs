f343b1990d2b0e010cf94ff16dc8fb27a17b6695
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-02-11 20:37:07 +0000

    props added

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,19 @@
-function UserProfile({ name, age, bio }) {
+import PropTypes from 'prop-types';
+
+const UserProfile = (props) => {
   return (
     <div>
-      <h2>{`name`}</h2>
-      <p>Age: {`age`}</p>
-      <p>Bio: {`bio`}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
-}
+};
 
-export default UserProfile
+UserProfile.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  bio: PropTypes.string.isRequired,
+};
+
+export default UserProfile;
