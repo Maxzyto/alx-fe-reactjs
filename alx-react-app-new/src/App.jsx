054f5fc35a80bdc67782462ac8f44c844b27ebc0
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-02-22 05:22:13 +0000

    counter added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import UserProfile from './components/UserProfile'
 import WelcomeMessage from './components/WelcomeMessage'
+import Counter from "./components/Counter"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <Footer />
     </>
   );
