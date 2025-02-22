34a66652992d96dd3a7ce50c7fb05f569ce387e0
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-02-22 05:06:35 +0000

    add new folder

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <footer
+    style={{
+      backgroundColor: "navy", color: "white", textAlign: "center", padding: "3px", position: "relative", marginTop: "10px", bottom: "0", width: "100%"}}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+
+
+function MainContent() {
+  return (
+    <main
+    style={{ backgroundColor: "lightgray", color: "black", padding: "5px", textAlign: "center", borderRadius: "10px" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent
