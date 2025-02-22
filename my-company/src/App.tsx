ff4206e23f3bc0ad3e6b008eb5905740514f0bab
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-02-22 21:25:57 +0000

    added files

--- my-company/src/App.tsx
+++ my-company/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Navbar = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <nav style={{ padding: "10px", backgroundColor: "#333", color: "white"}}>
+      <Link style={{ margin: "10px", color: "white" }} to="/">
+        Home
+      </Link>
+      <Link style={{ margin: "10px", color: "white" }} to="/about">
+        About
+      </Link>
+      <Link style={{ margin: "10px", color: "white" }} to="/services">
+        Services
+      </Link>
+      <Link style={{ margin: "10px", color: "white" }} to="/contact">
+        Contact
+      </Link>
+    </nav>
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes >
+        <Route path="/" element={<Home  />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
