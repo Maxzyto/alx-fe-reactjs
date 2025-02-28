89fa4841ee37e6ae726e5e7a6643692e698319a6
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-02-28 02:07:13 +0000

    new files

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import AddRecipeForm from './components/AddRecipeForm'
+import './App.css'
+import RecipeList from './components/RecipeList'
+
+function App() {
+
+
+  return (
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  )
+}
+
+export default App
