5f9454e5cc5730be32fb9eba30c80d8c312f730b
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-09 13:07:37 +0000

    name corrected formikForm.js

--- form-handling-react/src/router/router.js
+++ form-handling-react/src/router/router.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import RegistrationForm from '../components/RegistrationForm';
-import FormikForm from '../components/formiForm';
+import FormikForm from '../components/formikForm';
 import Login from '../pages/LoginPage';
 
 const Router = createBrowserRouter([
