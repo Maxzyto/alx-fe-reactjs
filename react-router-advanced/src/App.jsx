24785b0f7a01025e73ebb5824829f91b5d334858
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-09 15:08:01 +0000

    all components added to the app.js file

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import BlogPost from "./pages/BlogPost";
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Profile from "./components/Profile";
+import ProfileDetails from "./pages/profile/ProfileDetails";
+import ProfileSettings from "./pages/profile/ProfileSettings";
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
         <Route path="/blog/:id" element={<BlogPost />} />
         <Route path="*" element={<NotFound />} />
         <Route path="./components/ProtectedRoute.jsx" element={<ProtectedRoute />} />
+        <Route path='./pages/Profile.jsx' element={<Profile />} />
+        <Route path='./pages/profile/ProfileDetails.jsx' element={<ProfileDetails />} />
+        <Route path='./pages/profile/ProfileSettings.jsx' element={<ProfileSettings />} />
+
       </Routes>
     </Router>
   );
