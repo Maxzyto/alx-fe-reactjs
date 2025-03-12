683d55bd44136e7ff7e54823148dc3d1c2c5b045
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-12 18:44:16 +0000

    image and paragraph responsive

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,12 +4,11 @@ function UserProfile() {
       <img
         src="https://unsplash.com/photos/closeup-photography-of-woman-smiling-mEZ3PoFGs_k"
         alt="User"
-        className="rounded-full w-36 h-36 mx-out"
-        height={20}
-        width={20}
+        className="rounded-full w-36 sm:h-24 mx-out sm:w-24 md:w-36 md:h-36"
+        
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-lx">John Doe</h1>
+      <p className="text-gray-600 text-base sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
