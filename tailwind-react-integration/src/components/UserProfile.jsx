ffc98455b52421149cb9a0faa323d0b32adb0035
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-12 19:45:29 +0000

    image half fixed

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,13 @@
+import React from 'react';
+// import { Img } from 'react-image';
+
 function UserProfile() {
+
+  const imageUrl = "https://images.app.goo.gl/HoAhzD2tpzLFQqk49";
   return (
     <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm md:text-xl mx-auto my-20 rounded-lg shadow-lg">
       <img
-        src="https://unsplash.com/photos/closeup-photography-of-woman-smiling-mEZ3PoFGs_k"
+        src={imageUrl}
         alt="User"
         className="rounded-full w-36 sm:h-24 mx-out sm:w-24 md:w-36 md:h-36"
         
