0ef6854d2cea4bd0a1f5a72ee1a0f91f712a2b41
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-27 05:12:59 +0000

    github api imported

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,9 +4,10 @@ function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Enter a GitHub username to search for the user's profile</p>
+    </div>
   )
 }
 
